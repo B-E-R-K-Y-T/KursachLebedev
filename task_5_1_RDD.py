af7d3731eed04226e237ae241096a91ebf08e9b4
Tim <BerkyT115@yandex.ru>
2023-01-09 22:24:47 +0300

    Улучшил сохранение файла в спарк

--- task_5_1_RDD.py
+++ task_5_1_RDD.py
@@ -82,8 +82,7 @@ def main():
                                                     rec[is_retweet_index]))
 
     # Сохраняем результат в файл PATH_TO_FILE_RESULT в ФС
-    result_tuples = result_tuples.toDF().toJSON()
-    result_tuples.saveAsTextFile(PATH_TO_FILE_RESULT)
+    result_tuples.save_as_text_file_obj(PATH_TO_FILE_RESULT)
 
 
 if __name__ == '__main__':
